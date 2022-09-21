b4dea6cbccd8afe7cba5774c4d1d3aa8485ebda2
Matias Emanuel Tande <matiasetande@gmail.com>
2022-09-21 13:41:13 -0300

    Se agregan las clases Persona, Profesor, Estudiante y la interface Informacion

---
+++ src/Estudiante.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Estudiante extends Persona{
+
+    //Atributos
+
+    //Constructor
+
+    protected Estudiante(String nombre, String apellido, int legajo) {
+        super(nombre, apellido, legajo);
+    }
+
+    //Setter
+
+    //Getter
+
+    //Metodos
+
+
+    @Override
+    public String toString() {
+        return "Estudiante: {\n"+super.toString()+"}";
+    }
+
+    @Override
+    public void modificarDatos() {
+        int opcion = -1;
+
+        while (opcion != 0){
+
+            System.out.println("¿Que datos desea Modificar?");
+            System.out.println  ("1: Nombre" +
+                               "\n2: Apellido" +
+                               "\n3: Legajo" +
+                               "\n0: Salir" +
+                               "\nIngrese su opcion: ");
+
+            Scanner leer = new Scanner(System.in);
+            opcion =leer.nextInt();
+
+            switch (opcion){
+                case 1: System.out.println("modificando nombre");break;
+                case 2: System.out.println("modificando Apellido");break;
+                case 3: System.out.println("modificando legajo");break;
+                case 0: System.out.println("Saliendo de modificaciones");break;
+                default:System.out.println("Error: porfavor ingrese una opcion valida");break;
+            }
+        }
+
+
+    }
+}
