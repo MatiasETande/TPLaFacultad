a67556bb00dc9b93fcbd0841c57cd1a26153c51e
Matias Emanuel Tande <matiasetande@gmail.com>
2022-09-20 22:31:35 -0300

    Se crea repositorio del proyecto

---
+++ src/LaFacultadTP.java
@@ -0,0 +1,17 @@
+/*
+*
+*   Trabajo Practico realizadopara la materia Lab de computación II
+*   de la carrera TUP UTN FRRe
+*
+*   By Mazza Bruno & Tande Matias
+*
+* */
+
+public class LaFacultadTP {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
